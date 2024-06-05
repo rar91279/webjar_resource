795fc8ed6135cb364d636bd89780fe16e783220b
Roman Mertyn <r.mertyn@betterbits.de>
2024-06-05 16:02:15 +0200

    Initial commit

---
+++ src/main/kotlin/de/betterbits/r_mertyn/ExampleResource.kt
@@ -0,0 +1,14 @@
+package de.betterbits.r_mertyn
+
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.MediaType
+
+@Path("/hello")
+class ExampleResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    fun hello() = "Hello from Quarkus REST"
+}
---
+++ src/test/kotlin/de/betterbits/r_mertyn/ExampleResourceIT.kt
@@ -0,0 +1,6 @@
+package de.betterbits.r_mertyn
+
+import io.quarkus.test.junit.QuarkusIntegrationTest
+
+@QuarkusIntegrationTest
+class ExampleResourceIT : ExampleResourceTest()
